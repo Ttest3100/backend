5a7962fac4784eb7b9fb27ba4c0189e178e99f69
Ttest3100 <tulyarth.testing@gmail.com>
2026-07-06 15:55:27 +0530

    Changes Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 // Load environment variables
 dotenv.config({
